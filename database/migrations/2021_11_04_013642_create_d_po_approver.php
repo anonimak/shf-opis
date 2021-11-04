6d818968ae9ef431005722d0930ef5a837f143d2
anonimak <sanonimak@gmail.com>
2021-11-04 10:21:41 +0700

    [OPIS/21210417]

---
+++ database/migrations/2021_11_04_013642_create_d_po_approver.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDPoApprover extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('d_po_approver', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_memo');
+            $table->unsignedBigInteger('id_employee');
+            $table->integer('idx');
+            $table->text('msg')->nullable();
+            $table->enum('status', ['submit', 'edit', 'reject', 'approve', 'revisi'])->default('edit');
+            $table->timestamps();
+
+            // foreign key
+            $table->foreign('id_memo')->references('id')->on('m_memos')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_employee')->references('id')->on('m_employee')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('d_po_approver');
+    }
+}
