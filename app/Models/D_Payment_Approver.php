876138bd275bb7aa3fa0028ef3d88eeef7476cd4
anonimak <sanonimak@gmail.com>
2021-11-04 10:21:16 +0700

    [OPIS/21210418]

---
+++ app/Models/D_Payment_Approver.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class D_Payment_Approver extends Model
+{
+    protected $table = 'd_payment_approver';
+
+    protected $guarded = [];
+}
