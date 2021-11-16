c6c34a771324f61984f5db443aa066663dedd0c2
anonimak <sanonimak@gmail.com>
2021-11-16 11:43:43 +0700

    migration update_table_d_po_approver

---
+++ database/migrations/2021_11_16_044223_update_table_d_po_approver.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTableDPoApprover extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('d_po_approvers', function (Blueprint $table) {
+            $table->enum('type_approver', ['approver', 'acknowledge'])->default('approver');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('d_po_approvers', function (Blueprint $table) {
+            $table->dropColumn('type_approver');
+        });
+    }
+}
