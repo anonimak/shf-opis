ca890f813bed3722dbb98225225d3b07953c2997
anonimak <sanonimak@gmail.com>
2021-11-17 14:30:57 +0700

    [OPIS/21210441]

---
+++ resources/js/Pages/User/Status_Po/index.vue
@@ -0,0 +1,294 @@
+<template>
+  <layout :userinfo="userinfo" :notif="notif">
+    <flash-msg />
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Status PO</h1>
+    </div>
+    <breadcrumb :items="breadcrumbItems" />
+    <div class="row">
+      <div class="col-12">
+        <div>
+          <b-card>
+            <keep-alive>
+              <div class="row">
+                <div class="col-12">
+                  <b-tabs
+                    content-class="mt-3"
+                    align="center"
+                    v-model="tabIndex"
+                    @activate-tab="activeTab"
+                    small
+                  >
+                    <b-tab>
+                      <template #title>
+                        On Process
+                        <b-badge v-if="counttab.submit > 0" variant="primary">{{
+                          counttab.submit
+                        }}</b-badge>
+                      </template>
+                    </b-tab>
+                    <b-tab>
+                      <template #title>
+                        Approved
+                        <b-badge
+                          v-if="counttab.approve > 0"
+                          variant="primary"
+                          >{{ counttab.approve }}</b-badge
+                        >
+                      </template>
+                    </b-tab>
+                    <b-tab>
+                      <template #title>
+                        Rejected
+                        <b-badge v-if="counttab.reject > 0" variant="primary">{{
+                          counttab.reject
+                        }}</b-badge>
+                      </template>
+                    </b-tab>
+                  </b-tabs>
+                  <div class="row"></div>
+                  <div class="col-lg-3 col-xs-12 mt-3">
+                    <search v-model="form.search" @reset="reset" />
+                  </div>
+                  <div class="table-responsive">
+                    <b-overlay
+                      :show="isLoadMemo"
+                      opacity="0.6"
+                      spinner-small
+                      spinner-variant="primary"
+                    >
+                      <table class="table mt-4">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Document No</th>
+                            <th scope="col">Status</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody v-if="dataMemo.data != undefined">
+                          <tr
+                            v-for="(item, index) in dataMemo.data"
+                            :key="item.id"
+                          >
+                            <th scope="row">
+                              {{
+                                (filters.page !== undefined
+                                  ? filters.page - 1
+                                  : 1 - 1) *
+                                  perPage +
+                                index +
+                                1
+                              }}
+                            </th>
+                            <td>
+                              {{ item.title }}
+                            </td>
+                            <td>
+                              {{ item.doc_no }}
+                            </td>
+                            <td>
+                              {{ item.latest_history.content }}
+                            </td>
+                            <td>
+                              <inertia-link
+                                v-if="tabIndex == 3"
+                                :href="route(__senddraft, item.id)"
+                                class="btn btn-warning btn-sm my-2"
+                              >
+                                send to draft
+                              </inertia-link>
+                              <inertia-link
+                                :href="route(__webpreview, item.id)"
+                                class="btn btn-secondary btn-sm"
+                              >
+                                preview
+                              </inertia-link>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </b-overlay>
+                  </div>
+                  <Pagination
+                    v-if="dataMemo.links != undefined"
+                    :links="dataMemo.links"
+                  />
+                </div>
+              </div>
+            </keep-alive>
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </layout>
+</template>
+<script>
+import Layout from "@/Shared/UserLayout"; //import layouts
+import FlashMsg from "@/components/Alert";
+import Breadcrumb from "@/components/Breadcrumb";
+import Pagination from "@/components/Pagination";
+import Search from "@/components/Search";
+import throttle from "lodash/throttle";
+import pickBy from "lodash/pickBy";
+import mapValues from "lodash/mapValues";
+import { Timeline, TimelineItem, TimelineTitle } from "vue-cute-timeline";
+export default {
+  props: [
+    "meta",
+    "flash",
+    "breadcrumbItems",
+    "dataMemo",
+    "userinfo",
+    "notif",
+    "filters",
+    "perPage",
+    "tab",
+    "counttab",
+    "__create",
+    "__edit",
+    "__show",
+    "__destroy",
+    "__indexpayment",
+    "__webpreview",
+    "__senddraft",
+  ],
+  metaInfo: { title: "Admin Reference Approve Page" },
+  data() {
+    return {
+      tabIndex: 0,
+      form: {
+        search: this.filters.search,
+      },
+      // memo: { data: [], link: [] },
+      isLoadMemo: false,
+      isCheched: false,
+    };
+  },
+  components: {
+    Layout,
+    FlashMsg,
+    Breadcrumb,
+    Pagination,
+    Search,
+    Timeline,
+    TimelineItem,
+    TimelineTitle,
+  },
+  beforeMount() {
+    this.setLsTabMemo();
+  },
+  methods: {
+    submitDelete(id) {
+      this.$inertia.delete(route(this.__destroy, id));
+    },
+    submitDeleteAll(idx) {
+      //   this.$inertia.delete(route("admin.post.news.delete-all", idx.join()));
+    },
+    showMsgBoxDelete: function (id) {
+      this.$bvModal
+        .msgBoxConfirm(
+          "Please confirm that you want to delete this reference approver.",
+          {
+            title: "Please Confirm",
+            size: "sm",
+            buttonSize: "sm",
+            okVariant: "danger",
+            okTitle: "YES",
+            cancelTitle: "NO",
+            footerClass: "p-2",
+            hideHeaderClose: false,
+            centered: true,
+          }
+        )
+        .then((value) => {
+          value && this.submitDelete(id);
+        })
+        .catch((err) => {
+          // An error occurred
+        });
+    },
+    showMsgBoxDeleteAll: function () {
+      this.$bvModal
+        .msgBoxConfirm(
+          "Please confirm that you want to delete this checked post.",
+          {
+            title: "Please Confirm",
+            size: "sm",
+            buttonSize: "sm",
+            okVariant: "danger",
+            okTitle: "YES",
+            cancelTitle: "NO",
+            footerClass: "p-2",
+            hideHeaderClose: false,
+            centered: true,
+          }
+        )
+        .then((value) => {
+          // value && this.submitDeleteAll(this.selected);
+        })
+        .catch((err) => {
+          // An error occurred
+        });
+    },
+    uncheckAll: function () {
+      // this.selected = []
+    },
+    reset() {
+      this.form = mapValues(this.form, () => null);
+    },
+    setLsTabMemo() {
+      this.isLoadMemo = true;
+      // this.memo = { data: [], link: [] };
+      if (this.$ls.get("tabIndexMemo")) {
+        this.tabIndex = this.$ls.get("tabIndexMemo") - 1;
+      }
+
+      let param = { tab: this.tab[this.tabIndex] };
+      if (this.filters.page) {
+        param.page = this.filters.page;
+      }
+
+      this.$inertia.replace(route(this.__index, param)).then(() => {
+        // this.memo = { ...this.dataMemo };
+        this.isLoadMemo = false;
+      });
+    },
+    activeTab(tabIndex) {
+      this.tabIndex = tabIndex;
+      this.isLoadMemo = true;
+      // this.memo = { data: [], link: [] };
+      this.$ls.set("tabIndexMemo", this.tabIndex + 1, 60 * 60 * 1000);
+
+      let param = { tab: this.tab[tabIndex] };
+      if (this.filters.page) {
+        param.page = this.filters.page;
+      }
+      this.$inertia.replace(route(this.__index, param)).then(() => {
+        // this.memo = { ...this.dataMemo };
+        this.isLoadMemo = false;
+      });
+    },
+  },
+  watch: {
+    form: {
+      handler: throttle(function () {
+        let query = this.form.search;
+        this.$inertia.replace(
+          this.route(
+            this.__index,
+            Object.keys(query).length
+              ? { search: query, tab: this.tab[this.tabIndex] }
+              : {
+                  remember: "forget",
+                  tab: this.tab[this.tabIndex],
+                }
+          )
+        );
+      }, 150),
+      deep: true,
+    },
+  },
+};
+</script>
---
+++ resources/js/Pages/User/Status_Po/preview.vue
@@ -0,0 +1,312 @@
+<template>
+  <layout :userinfo="userinfo" :notif="notif">
+    <flash-msg />
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Memo {{ dataMemo.doc_no }}</h1>
+    </div>
+    <breadcrumb :items="breadcrumbItems" />
+    <div class="row">
+      <div class="col-12">
+        <b-card no-body>
+          <b-card-body>
+            <b-row class="mb-2">
+              <b-col col lg="6" md="auto">
+                <h5>Memo Information</h5>
+                <table class="table table-bordered">
+                  <tbody>
+                    <tr>
+                      <td>Proposed By</td>
+                      <td>
+                        {{
+                          proposeEmployee.firstname +
+                          " " +
+                          proposeEmployee.lastname
+                        }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Status</td>
+                      <td>
+                        <b-badge
+                          v-if="dataMemo.status_payment == 'submit'"
+                          variant="info"
+                          >On process approving</b-badge
+                        >
+                        <b-badge
+                          v-if="dataMemo.status_payment == 'approve'"
+                          variant="success"
+                          >Memo Approved</b-badge
+                        >
+                        <b-badge
+                          v-if="dataMemo.status_payment == 'reject'"
+                          variant="danger"
+                          >Memo Rejected</b-badge
+                        >
+                        <b-badge
+                          v-if="dataMemo.status_payment == 'revisi'"
+                          variant="secondary"
+                          >Memo Revisi</b-badge
+                        >
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Department</td>
+                      <td>
+                        {{
+                          proposeEmployee.emp_history.position.department
+                            .department_name
+                        }}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Title</td>
+                      <td>{{ dataMemo.title }}</td>
+                    </tr>
+                    <tr>
+                      <td>Doc. No</td>
+                      <td>{{ dataMemo.doc_no }}</td>
+                    </tr>
+                    <tr>
+                      <td>Type</td>
+                      <td>Approval</td>
+                    </tr>
+                    <tr v-if="dataMemo.acknowledges.length > 0">
+                      <td>Acknowledge</td>
+                      <td>
+                        <span
+                          v-for="(acknowledge, index) in dataMemo.acknowledges"
+                          :key="index"
+                        >
+                          {{
+                            acknowledge.employee.firstname +
+                            " " +
+                            acknowledge.employee.lastname
+                          }}
+                          <span v-if="index != dataMemo.acknowledges.length - 1"
+                            >,{{ " " }}
+                          </span>
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </b-col>
+              <b-col col lg="6" md="auto">
+                <h5>Approver</h5>
+                <table class="table table-bordered mb-2">
+                  <thead class="thead-dark">
+                    <tr>
+                      <th>Level</th>
+                      <th>Approver Name</th>
+                      <th>Position</th>
+                      <th>Approver Type</th>
+                      <th>Status</th>
+                      <th>Message</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="(approver, index) in dataMemo.approvers_po"
+                      :key="index"
+                    >
+                      <td>
+                        {{ index + 1 }}
+                      </td>
+                      <td>
+                        {{
+                          approver.employee.firstname +
+                          " " +
+                          approver.employee.lastname
+                        }}
+                      </td>
+                      <td>
+                        {{
+                          approver.employee.position_now.position.position_name
+                        }}
+                      </td>
+                      <td>
+                        {{ approver.type_approver }}
+                      </td>
+                      <td>
+                        <b-badge
+                          v-if="approver.status == 'submit'"
+                          variant="info"
+                          >On Submit</b-badge
+                        >
+                        <b-badge
+                          v-if="approver.status == 'approve'"
+                          variant="success"
+                          >Approved</b-badge
+                        >
+                        <b-badge
+                          v-if="approver.status == 'reject'"
+                          variant="danger"
+                          >Rejected</b-badge
+                        >
+                        <b-badge
+                          v-if="approver.status == 'revisi'"
+                          variant="secondary"
+                          >Revisi</b-badge
+                        >
+                      </td>
+                      <td>
+                        <p v-if="approver.msg">{{ approver.msg }}</p>
+                        <span v-else>-</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div
+                  class="card mb-4"
+                  v-if="dataMemo && dataMemo.histories.length > 0"
+                >
+                  <div
+                    class="
+                      card-header
+                      py-3
+                      d-flex
+                      flex-row
+                      align-items-center
+                      justify-content-between
+                    "
+                  >
+                    <h6 class="m-0 font-weight-bold text-primary">History</h6>
+                  </div>
+                  <!-- Card Body -->
+                  <div class="card-body">
+                    <div class="overflow-auto" style="height: 218px">
+                      <timeline>
+                        <timeline-item
+                          v-for="(itemHistory, index) in dataMemo.histories"
+                          :key="index"
+                          :bg-color="timelinecolor[itemHistory.type]"
+                        >
+                          <strong>
+                            {{ itemHistory.title }}
+                            <span class="float-right">
+                              <small class="text-muted">
+                                <em>
+                                  {{
+                                    itemHistory.created_at
+                                      | moment("D/M/YY,h:mm a")
+                                  }}
+                                </em>
+                              </small>
+                            </span>
+                          </strong>
+                          <p>
+                            <small class="text-muted">{{
+                              itemHistory.content
+                            }}</small>
+                          </p>
+                        </timeline-item>
+                      </timeline>
+                    </div>
+                  </div>
+                </div>
+              </b-col>
+            </b-row>
+            <b-row
+              v-if="dataMemo.background && dataMemo.background != '<p></p>'"
+              class="mb-2"
+            >
+              <b-col>
+                <h5>Background</h5>
+                <div v-html="dataMemo.background"></div>
+              </b-col>
+            </b-row>
+            <b-row
+              v-if="dataMemo.information && dataMemo.information != '<p></p>'"
+              class="mb-2"
+            >
+              <b-col>
+                <h5>Information</h5>
+                <div v-html="dataMemo.information"></div> </b-col
+            ></b-row>
+            <b-row
+              v-if="dataMemo.conclusion && dataMemo.conclusion != '<p></p>'"
+              class="mb-2"
+            >
+              <b-col>
+                <h5>Conclusion</h5>
+                <div v-html="dataMemo.conclusion"></div> </b-col
+            ></b-row>
+            <b-row v-if="memocost.length > 0" class="mb-2">
+              <b-col>
+                <h5>Cost/Expense</h5>
+                <b-table bordered :items="memocost"></b-table> </b-col
+            ></b-row>
+            <b-row
+              v-if="dataMemo.payment && dataMemo.payment != '<p></p>'"
+              class="mb-2"
+            >
+              <b-col>
+                <h5>Payment</h5>
+                <div v-html="dataMemo.payment"></div>
+              </b-col>
+            </b-row>
+            <b-row v-if="attachments.length > 0" class="mb-2">
+              <b-col>
+                <h5>Attachment</h5>
+                <table class="table table-bordered">
+                  <thead class="thead-dark">
+                    <tr>
+                      <th>file</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(attachment, index) in attachments" :key="index">
+                      <a
+                        :href="attachment.name"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {{ attachment.real_name }}
+                      </a>
+                    </tr>
+                  </tbody>
+                </table>
+              </b-col>
+            </b-row>
+          </b-card-body>
+        </b-card>
+      </div>
+    </div>
+  </layout>
+</template>
+<script>
+import Layout from "@/Shared/UserLayout"; //import layouts
+import FlashMsg from "@/components/Alert";
+import Breadcrumb from "@/components/Breadcrumb";
+import { Timeline, TimelineItem, TimelineTitle } from "vue-cute-timeline";
+
+export default {
+  props: [
+    "userinfo",
+    "notif",
+    "breadcrumbItems",
+    "dataMemo",
+    "proposeEmployee",
+    "memocost",
+    "attachments",
+  ],
+  components: {
+    Layout,
+    FlashMsg,
+    Breadcrumb,
+    Timeline,
+    TimelineItem,
+    TimelineTitle,
+  },
+  data() {
+    return {
+      timelinecolor: {
+        success: "#1cc88a",
+        info: "#36b9cc",
+        danger: "#e74a3b",
+        warning: "#f6c23e",
+      },
+    };
+  },
+};
+</script>
