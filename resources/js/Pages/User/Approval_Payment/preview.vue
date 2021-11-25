5913bdb13a0b4e14c6d80f8e6e15cb52bc712eb5
Clarissa <56617602+clrssanbl@users.noreply.github.com>
2021-11-25 14:11:19 +0700

    updated format number to display data amount from memo payment

--- resources/js/Pages/User/Approval_Payment/preview.vue
+++ resources/js/Pages/User/Approval_Payment/preview.vue
@@ -232,7 +232,7 @@
                       <td>{{ item.name }}</td>
                       <td>{{ item.bank_name }}</td>
                       <td>{{ item.bank_account }}</td>
-                      <td>{{ item.amount }}</td>
+                      <td>{{ Number(item.amount).toLocaleString() }}</td>
                       <td>{{ item.remark }}</td>
                     </tr>
                   </tbody>
