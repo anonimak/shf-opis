7c000e939a8ca625bccb9edd5eda53ad2ac124f8
Clarissa <56617602+clrssanbl@users.noreply.github.com>
2022-03-25 10:26:32 +0700

    OPIS-18 module edit takeover & type memo in Super Admin

--- app/Http/Controllers/User/ApiMemoController.php
+++ app/Http/Controllers/User/ApiMemoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\D_Memo_Approver;
 use App\Models\Employee_History;
+use App\Models\Ref_Type_Memo;
+use App\Models\Memo;
 use Illuminate\Http\Request;
 
 class ApiMemoController extends Controller
@@ -34,5 +36,35 @@ class ApiMemoController extends Controller
         }])->orderBy('idx', 'asc')->get();
 
         return response()->json($approvers);
+    }
+
+    public function getTypeMemoByIdMemo($id)
+    {
+        $proposeEmployee = Memo::with(['proposeemployee' => function ($employee) {
+            return $employee->select('id', 'firstname', 'lastname')->with(['position_now' => function ($position_now) {
+                return $position_now->with(['position' => function ($position) {
+                    return $position->with('department');
+                }])->with('branch');
+            }]);
+        }])
+            ->where('id', $id)->first();
+
+        $isHeadOffice =  $proposeEmployee->proposeemployee->position_now->branch->is_head;
+        if (!$isHeadOffice) {
+            $dataTypeMemo = Ref_Type_Memo::whereNull('id_department')->orWhere(function ($query) use ($proposeEmployee) {
+                $query->where('id_department', $proposeEmployee->proposeemployee->position_now->position->id_department);
+                $query->where('id_branch', $proposeEmployee->proposeemployee->position_now->branch->id);
+            })
+                ->orderBy('id_department', 'asc')->get();
+        } else {
+            $dataTypeMemo = Ref_Type_Memo::whereNull('id_department')
+                ->orWhere(function ($query) use ($proposeEmployee) {
+                    $query->where('id_department', $proposeEmployee->proposeemployee->position_now->position->id_department);
+                })
+                ->orderBy('id_department', 'asc')->get();
+        }
+
+
+        return response()->json($dataTypeMemo);
     }
 }
