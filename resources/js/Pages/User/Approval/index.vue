f0c747be2d96b6ccb5546b9b449674b322b73f6d
Clarissa <56617602+clrssanbl@users.noreply.github.com>
2022-04-28 11:42:59 +0700

    update bug search
    update bug search in tab all status and approval index

--- resources/js/Pages/User/Approval/index.vue
+++ resources/js/Pages/User/Approval/index.vue
@@ -471,8 +471,8 @@ export default {
       if (this.$ls.get("tabIndexApprovalMemo")) {
         this.tabIndex = this.$ls.get("tabIndexApprovalMemo") - 1;
       }
-
-      let param = { tab: this.tab[this.tabIndex] };
+      let query = this.form.search;
+      let param = { search: query, tab: this.tab[this.tabIndex] };
       if (this.filters.page) {
         param.page = this.filters.page;
       }
